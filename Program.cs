e81c19966b6a34106f2dc857c94308faee336516
Kate <kate@gmail.com>
2023-03-14 22:08:54 +0300

    Задан массив

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// Вариант решения 1:
+
+Console.WriteLine("Задан массив:");
+string[] array = {"moscow", "spb", "mjz", "irkutst", "frankfurt"};
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " , ");
+        Console.WriteLine();
